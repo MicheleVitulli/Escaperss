8b9644fda744ffe98d287ebf035c3f93ff018110
Michele Vitulli <vitulli430@gmail.com>
2021-05-18 18:28:03 +0200

    18 maggio 18:40

--- app/controllers/escapes_controller.rb
+++ app/controllers/escapes_controller.rb
@@ -1,4 +1,7 @@
 class EscapesController < ApplicationController
+    
+    skip_before_action :verify_authenticity_token
+
     def index
         @all = Escape.all
         #render json: Escape.all
@@ -12,5 +15,27 @@ class EscapesController < ApplicationController
         end
         
     end
+    def new
+        @escape = Escape.new
+    end
+
+    def create
+        @escape = Escape.new(escape_params)
+        if @escape.save
+            redirect_to @escape
+        else
+            render :new
+        end
+        #edirect_to escapes_path
+    end
+    def escape_params
+        params.require(:escape).permit(:Luogo, :Stanza)
+    end
+    def destroy
+        @escape = Escape.find(params[:id])
+        @escape.destroy
+    
+        redirect_to escapes_path
+      end
         
 end
